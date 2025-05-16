8884c948b4416eb5bb587c66e0b344c1a6ce3c8a
jonas pauli <jonaspauli089@gmail.com>
2025-05-16 23:07:23 +0200

    update blueprint

--- recursion/circuit/src/blueprint.rs
+++ recursion/circuit/src/blueprint.rs
@@ -98,26 +98,21 @@ pub fn main() {
         // there might be a new sync committee in charge moving forward,
         // if that is the case we want to update our service state to reflect that
         // rotation
-        let mut next_active_sync_committee: [u8; 32] = recursive_proof_outputs.active_committee;
-
-        // if there is a new sync committee, we want to commit it for the next round
-        if helios_output.nextSyncCommitteeHash != [0u8; 32] {
-            next_active_sync_committee = helios_output
-                .nextSyncCommitteeHash
-                .to_vec()
-                .try_into()
-                .expect("Failed to fit committeeHash into slice");
-        }
+        let new_proof_active_committee: [u8; 32] = helios_output
+            .syncCommitteeHash
+            .to_vec()
+            .try_into()
+            .expect("Failed to fit committeeHash into slice");
 
         // Assert that the previous committee of the new proof matches the expected active committee
         assert_eq!(
-            helios_output.syncCommitteeHash,
+            helios_output.prevSyncCommitteeHash,
             recursive_proof_outputs.active_committee
         );
 
         // Commit the outputs required by the wrapper circuit
         let outputs = RecursionCircuitOutputs {
-            active_committee: next_active_sync_committee,
+            active_committee: new_proof_active_committee,
             root: state_root.to_vec().try_into().unwrap(),
             height: unpad_block_number(&height),
             vk: inputs.recursive_vk,
