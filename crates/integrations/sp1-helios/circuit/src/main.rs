ce3a9a63c8edfe3cb4db744e8ff971bf42165531
jonas pauli <jonaspauli089@gmail.com>
2025-06-22 00:59:58 +0200

    remove previous committee from constraint and re-compile elfs

--- crates/integrations/sp1-helios/circuit/src/main.rs
+++ crates/integrations/sp1-helios/circuit/src/main.rs
@@ -119,42 +119,8 @@ fn get_helios_outputs(
         // the new head must be greater than the previous head
         assert!(helios_output.prevHead < helios_output.newHead);
 
-        // if the new head is for a new perid, the previous committee hash must match
-        // the previous committee hash of the previous proof
-        if helios_output.prevHead / U256::from(8192) < helios_output.newHead / U256::from(8192) {
-            if helios_output.prevSyncCommitteeHash != recursive_proof_outputs.previous_committee {
-                println!(
-                    "new proof previous committee: {:?}",
-                    helios_output.prevSyncCommitteeHash
-                );
-                println!(
-                    "previous proof previous committee: {:?}",
-                    recursive_proof_outputs.previous_committee
-                );
-                println!(
-                    "active committee: {:?}",
-                    recursive_proof_outputs.active_committee
-                );
-                panic!("Sync committee mismatch!");
-            }
-        } else {
-            // if the new head is for the same period, the previous committee hash must match
-            // the active committee hash of the previous proof
-            if helios_output.prevSyncCommitteeHash != recursive_proof_outputs.active_committee {
-                println!(
-                    "new proof previous committee: {:?}",
-                    helios_output.prevSyncCommitteeHash
-                );
-                println!(
-                    "previous proof previous committee: {:?}",
-                    recursive_proof_outputs.previous_committee
-                );
-                println!(
-                    "active committee: {:?}",
-                    recursive_proof_outputs.active_committee
-                );
-                panic!("Sync committee mismatch!");
-            }
+        if helios_output.prevSyncCommitteeHash != recursive_proof_outputs.active_committee {
+            panic!("Sync committee mismatch!");
         }
     }
 
--- crates/integrations/sp1-helios/wrapper-circuit/src/main.rs
+++ crates/integrations/sp1-helios/wrapper-circuit/src/main.rs
@@ -9,7 +9,7 @@ use helios_recursion_types::{
 };
 use sp1_verifier::Groth16Verifier;
 
-const RECURSIVE_VK: &str = "0x00874b6aaa32bca7c478176c9c8c256f998b0900855a58bac339f6924577c675";
+const RECURSIVE_VK: &str = "0x0034e4a559df3be8975c94d57857e1e6fbfc4d26177b8f60ccd2dd86e75fd8c7";
 
 fn main() {
     // Get the Groth16 verification key for proof verification
